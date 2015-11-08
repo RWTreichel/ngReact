b5d5fb59de53dd3011539847ebf426fdaf8bbe0c
Richard Treichel <rwtreichel@gmail.com>
2015-11-07 21:39:51 -0500

    Add an example of querying the CodePen API

--- js/codefeed-controller.js
+++ js/codefeed-controller.js
@@ -1,4 +1,13 @@
 angular.module('codefeedApp')
-  .controller('CodeFeedCtrl', ['$scope', function ($scope) {
-    $scope.name = 'Dick Treichel';
-  }]);
+  .controller('CodeFeedCtrl', ['$scope', '$http',
+    function ($scope, $http) {
+      $scope.name = 'Dick Treichel';
+      $scope.languages = [];
+
+      _.each(['html', 'css', 'js'], function (extension) {
+        $http.get('http://codepen.io/RWTreichel/pen/bVmRPp.' + extension)
+          .success(function (response) {
+            $scope.languages.push(response);
+          });
+      });
+    }]);
