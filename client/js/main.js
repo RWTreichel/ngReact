d0cd694203e3f8348e5de43493b6d356b843ae63
Richard Treichel <rwtreichel@gmail.com>
2015-11-08 13:40:30 -0500

    Resolve a few merge conflicts. Add ngRoute capabilities.

--- client/js/main.js
+++ client/js/main.js
@@ -1,1 +1,11 @@
-var codefeedApp = angular.module('codefeedApp', ['react']);
+var codefeedApp = angular.module('codefeedApp', ['react', 'ngRoute']);
+
+// codefeedApp.config(['$routeProvider',
+//   function ($routeProvider) {
+//     $routeProvider
+//       .when('/', {
+//         templateUrl: 'linkToTemplate.html',
+//         controller: 'SomeCtrl',
+//         controllerAs: 'vm'
+//       });
+//   }]);
